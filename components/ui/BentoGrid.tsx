73afae53c5b86a3b01ce32dd72e0afdef1b76611
masuma131 <creative.masuma@gmail.com>
2025-01-03 02:50:57 -0500

    changed info

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -66,7 +66,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "masumabegum.swe@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
